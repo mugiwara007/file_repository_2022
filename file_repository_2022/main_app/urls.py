daddc515e338775d32a45eb08b9f7e264bb9808a
Josh <joshcruz2028@gmail.com>
2022-01-28 15:52:50 +0800

    User and Admin Change Password (DONE), User and Admin Edit Account (DONE), User and Admin Profile (DONE). User Archive (DONE), User Homepage (FIXED), Footer (FIXED), Sidebar (FIXED)

--- file_repository_2022/main_app/urls.py
+++ file_repository_2022/main_app/urls.py
@@ -4,7 +4,14 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('register/', views.register, name='register'),
+    path('Register/', views.register, name='register'),
     path('Admin/', views.AdminHomepage, name='AdminHomepage'),
     path('User/', views.UserHomepage, name='UserHomepage'),
+    path('UserProfile/', views.UserProfile, name='UserProfile'),
+    path('UserChangePassword/', views.UserChangePassword, name='UserChangePassword'),
+    path('UserEditAccount/', views.UserEditAccount, name='UserEditAccount'),
+    path('UserArchive/', views.UserArchive, name='UserArchive'),
+    path('AdminProfile/', views.AdminProfile, name='AdminProfile'),
+    path('AdminEditAccount/', views.AdminEditAccount, name='AdminEditAccount'),
+    path('AdminChangePassword/', views.AdminChangePassword, name='AdminChangePassword'),
 ]
--- file_repository_2022/main_app/views.py
+++ file_repository_2022/main_app/views.py
@@ -6,14 +6,33 @@ from django.http import HttpResponse
 def index(request):
     return render(request, 'index.html')
 
-
 def register(request):
     return render(request, 'register.html')
-
 
 def AdminHomepage(request):
     return render(request, 'AdminHomepage.html')
 
 def UserHomepage(request):
     return render(request, 'UserHomepage.html')
+
+def UserProfile(request):
+    return render(request, 'UserProfile.html')
+
+def UserChangePassword(request):
+    return render(request, 'UserChangePassword.html')
+
+def UserEditAccount(request):
+    return render(request, 'UserEditAccount.html')
+
+def UserArchive(request):
+    return render(request, 'UserArchive.html')
+
+def AdminProfile(request):
+    return render(request, 'AdminProfile.html')
+
+def AdminEditAccount(request):
+    return render(request, 'AdminEditAccount.html')
+
+def AdminChangePassword(request):
+    return render(request, 'AdminChangePassword.html')
     
