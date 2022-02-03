a62d4186cbd565d6382a6dd044a7f07ac914b78d
eminemz <emem.lopez2537@gmail.com>
2022-02-04 00:53:17 +0800

    done show/hide pass

---
+++ file_repository_2022/main_app/static/js/showpassRegister.js
@@ -0,0 +1,22 @@
+const togglePassword = document.querySelector('#togglePassword2');
+const password = document.querySelector('#Pass');
+
+togglePassword.addEventListener('click', function (e) {
+  // toggle the type attribute
+  const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
+  password.setAttribute('type', type);
+  // toggle the eye slash icon
+  this.classList.toggle('fa-eye-slash');
+});
+
+
+const togglePassword2 = document.querySelector('#togglePassword3');
+const password2 = document.querySelector('#CPass');
+
+togglePassword2.addEventListener('click', function (e) {
+  // toggle the type attribute
+  const type = password2.getAttribute('type') === 'password' ? 'text' : 'password';
+  password2.setAttribute('type', type);
+  // toggle the eye slash icon
+  this.classList.toggle('fa-eye-slash');
+});
