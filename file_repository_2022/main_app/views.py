32d4e06d646476b43eee37740191bccb8e845de4
Ain10 <iandominique.r.dejesus@gmail.com>
2022-02-08 03:04:24 +0800

    Session Redirect
    -Image load
    -Admin edit profile
    -Admin password

--- file_repository_2022/main_app/views.py
+++ file_repository_2022/main_app/views.py
@@ -28,10 +28,10 @@ def index(request):
             request.session['logged_id'] = found.id
             if(found.admin and request.POST['user-type']=="Admin"):
                 request.session['logged_user_type'] = found.admin
-                return render(request, 'AdminHomepage.html')
+                return redirect('AdminHomepage')
             elif(not found.admin and request.POST['user-type']=="User"):
                 request.session['logged_user_type'] = found.admin
-                return render(request, 'UserHomepage.html')
+                return redirect('UserHomepage')
         except:
             return render(request, 'index.html')
 #Redirect to page if user did not log out properly
@@ -94,6 +94,7 @@ def UserChangePassword(request):
         if request.POST['CurrentPass'] == found.password and request.POST['NewPass'] == request.POST['CNewPass']:
             found.password = request.POST['NewPass']
             found.save()
+            return redirect('UserProfile')
         else:
             #insert alert
             pass
@@ -108,23 +109,19 @@ def UserEditAccount(request):
     except:
         pass
     if request.method == 'POST':
-        try:
-            userEditFound = Profiles.objects.filter(eMail = request.POST['NewUserEmail'], username=request.POST['NewUserUsername'])
-            
-            #Data validation for existing accounts
-            if profile.password==request.POST['UserPassword']:
-                profile.username = request.POST['NewUserUsername']
-                profile.eMail = request.POST['NewUserEmail']
-                profile.save()
-                context['username'] = profile.username
-                context['email'] = profile.eMail
+
+        #Data validation for existing accounts
+        if profile.password==request.POST['UserPassword']:
+            profile.username = request.POST['NewUserUsername']
+            profile.eMail = request.POST['NewUserEmail']
+            profile.save()
+            try:
                 if(not request.FILES['UserPFP']==''):
                     profile.profile_picture = request.FILES['UserPFP']
                     profile.save()
-                context['picture'] = profile.profile_picture
-                
-        except:
-            pass
+            except:
+                pass
+            return redirect('UserProfile')
         
     return render(request, 'UserEditAccount.html',context)
 
@@ -142,23 +139,46 @@ def AdminProfile(request):
     user = Profiles.objects.get(id=request.session['logged_id'])
 
     context = {'username' : user.username,'email' : user.eMail,'profile_picture': user.profile_picture}
-    return render(request, 'AdminProfile.html')
+    return render(request, 'AdminProfile.html',context)
+
 
 def AdminEditAccount(request):
     if('logged_email' not in request.session):
         return render(request, 'index.html')
-    user = Profiles.objects.get(id=request.session['logged_id'])
+    admin = Profiles.objects.get(id=request.session['logged_id'])
+    
+    if request.method == 'POST':
+        #Data validation for existing accounts
+        if admin.password==request.POST['UserPassword']:
+            admin.username = request.POST['NewUserUsername']
+            admin.eMail = request.POST['NewUserEmail']
+            admin.save()
+            try:
+                if(not request.FILES['AdminPFP']==''):
+                    admin.profile_picture = request.FILES['AdminPFP']
+                    admin.save()
+            except:
+                pass
+            return redirect('AdminProfile')
 
-    context = {'username' : user.username,'email' : user.eMail,'profile_picture': user.profile_picture}
-    return render(request, 'AdminEditAccount.html')
+    context = {'username' : admin.username,'email' : admin.eMail,'profile_picture': admin.profile_picture}
+    return render(request, 'AdminEditAccount.html',context)
 
 def AdminChangePassword(request):
     if('logged_email' not in request.session):
         return render(request, 'index.html')
     user = Profiles.objects.get(id=request.session['logged_id'])
+    if request.method == 'POST':
+        if request.POST['CurrentPass'] == user.password and request.POST['NewPass'] == request.POST['CNewPass']:
+            user.password = request.POST['NewPass']
+            user.save()
+            return redirect('UserProfile')
+        else:
+            #insert alert
+            pass
 
-    context = {'username' : user.username,'email' : user.eMail,'profile_picture': user.profile_picture}
-    return render(request, 'AdminChangePassword.html')
+    context = {'profile_picture': user.profile_picture}
+    return render(request, 'AdminChangePassword.html',context)
 
 def AdminArchive(request):
     if('logged_email' not in request.session):
