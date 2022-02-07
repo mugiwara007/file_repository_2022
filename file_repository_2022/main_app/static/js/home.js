9bbafeeffb5216b8f2dc3e1683ce42749a3a64eb
janjersheyrelos <98583041+janjersheyrelos@users.noreply.github.com>
2022-02-07 22:24:06 +0800

    SweetAlertsDone!

--- file_repository_2022/main_app/static/js/home.js
+++ file_repository_2022/main_app/static/js/home.js
@@ -13,6 +13,114 @@ for (var i = 0; i < btnList.length; i++) {
     this.className += " active";
   });
 }
+function sweetalert(){
+  Swal.fire({
+    title: 'Are you sure?',
+    icon: 'warning',
+    showCancelButton: true,
+    confirmButtonColor: '#3085d6',
+    cancelButtonColor: '#d33',
+    confirmButtonText: 'Yes, Put this file in Archive'
+  }).then((result) => {
+    if (result.isConfirmed) {
+      Swal.fire(
+        'Archived',
+        'Your file has been sent to Archive.',
+        'success'
+      )
+    }
+  })
+}
+
+function deleteaccountalert(){
+  Swal.fire({
+    title: 'Are you sure?',
+    icon: 'warning',
+    showCancelButton: true,
+    confirmButtonColor: '#3085d6',
+    cancelButtonColor: '#d33',
+    confirmButtonText: 'Yes, Delete my Account'
+  }).then((result) => {
+    if (result.isConfirmed) {
+      Swal.fire(
+        'Deleted',
+        'Your Account has been Deleted',
+        'success'
+      )
+    }
+  })
+}
+function deleteuser(){
+  Swal.fire({
+    title: 'Are you sure you want to remove this User?',
+    icon: 'warning',
+    showCancelButton: true,
+    confirmButtonColor: '#3085d6',
+    cancelButtonColor: '#d33',
+    confirmButtonText: 'Delete this User'
+  }).then((result) => {
+    if (result.isConfirmed) {
+      Swal.fire(
+        'Archived',
+        'This user has been sent to Archive',
+        'success'
+      )
+    }
+  })
+}
+function deletefilepermanently(){
+  Swal.fire({
+    title: 'Do you want to Delete this file permenantly?',
+    icon: 'warning',
+    showCancelButton: true,
+    confirmButtonColor: '#3085d6',
+    cancelButtonColor: '#d33',
+    confirmButtonText: 'Delete File Permanently'
+  }).then((result) => {
+    if (result.isConfirmed) {
+      Swal.fire(
+        'Deleted',
+        'This File has been Deleted Permanently',
+        'success'
+      )
+    }
+  })
+}
+
+function restorefile(){
+  Swal.fire({
+    title: 'Do you want to Restore this file?',
+    showDenyButton: true,
+    confirmButtonText: 'Restore',
+  }).then((result) => {
+    /* Read more about isConfirmed, isDenied below */
+    if (result.isConfirmed) {
+      Swal.fire('Restored!', '', 'success')
+    } else if (result.isDenied) {
+      Swal.fire('File not Restored', '', 'info')
+    }
+  })
+}
+
+
+function deleteuserpermanently(){
+  Swal.fire({
+    title: 'Do you want to Delete this User permanently?',
+    icon: 'warning',
+    showCancelButton: true,
+    confirmButtonColor: '#3085d6',
+    cancelButtonColor: '#d33',
+    confirmButtonText: 'Delete User Permanently'
+  }).then((result) => {
+    if (result.isConfirmed) {
+      Swal.fire(
+        'Deleted',
+        'This User has been Deleted Permanently',
+        'success'
+      )
+    }
+  })
+}
 
 function userClick(){
   document.getElementById('user-type').value = "User";
@@ -22,3 +130,4 @@ function adminClick(){
   document.getElementById('user-type').value = "Admin";
   console.log(document.getElementById('user-type').value);
 }
+
